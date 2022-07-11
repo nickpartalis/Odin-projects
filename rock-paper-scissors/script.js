148b8fa540a5dee03ca631cbab6734f0e93e4151
Nick Partalis <nickpartalis@hotmail.com>
2022-07-11 20:35:27 +0300

    Add full game functionality

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -12,12 +12,37 @@ function computerPlay() {
 
 function playRound(playerChoice, compChoice) {
     playerChoice = playerChoice.toLowerCase();
-    if (playerChoice == compChoice) return "It's a tie!";
+    if (playerChoice == compChoice) return "tie";
     
     if ((playerChoice == "rock" && compChoice == "scissors") ||
         (playerChoice == "paper" && compChoice == "rock") ||
         (playerChoice == "scissors" && compChoice == "paper")
-    ) return `You win, ${playerChoice} beats ${compChoice}.`;
+    ) return "player";
 
-    return `You lose, ${compChoice} beats ${playerChoice}`;
+    return "computer";
 }
+
+function game() {
+    let playerScore = compScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Rock, paper or scissors? ");
+        let compChoice = computerPlay();
+        let result = playRound(playerChoice, compChoice);
+
+        if (result == "tie") console.log("It's a tie!");
+        else if (result == "player") {
+            ++playerScore;
+            console.log(`You win this round, ${playerChoice} beats ${compChoice}.`);
+        }
+        else {
+            ++compScore;
+            console.log(`You lose this round, ${compChoice} beats ${playerChoice}.`);
+        }
+    }
+    
+    if (playerScore > compScore) console.log(`Congratulations, you won ${playerScore}-${compScore}!`);
+    else if (compScore > playerScore) console.log(`Game over. You lost ${playerScore}-${compScore}.`);
+    else console.log (`Game is a draw, ${playerScore}-${compScore}.`);
+}
+
+game()
